5459a0eed8c800b04b18b8b9ac402b95133c2f51
jonas spenger <6832643+jonasspenger@users.noreply.github.com>
2020-03-26 21:59:32 +0100

    Update multichain-deployment.sh

--- evaluation/deployment/multichain/multichain-deployment.sh
+++ evaluation/deployment/multichain/multichain-deployment.sh
@@ -2,7 +2,7 @@ SCRIPTPATH=$(dirname "$0")
 
 # deploy etcd to kubernetes
 helm install multichainetcd bitnami/etcd --set auth.rbac.enabled=false,statefulset.replicaCount=6,nodeSelector.etcdnodetype=etcd,persistence.enabled=false;
-sleep 120
+sleep 180
 
 # delete multichain key from etcd
 kubectl exec statefulset/multichainetcd -- bash -c 'multichainetcd del multichain'
