8ea343c196eb65b16197a9c27969e8ca905fe2c2
jonas spenger <6832643+jonasspenger@users.noreply.github.com>
2020-04-07 11:30:00 +0200

    Update health_check.sh

--- evaluation/deployment/health_check.sh
+++ evaluation/deployment/health_check.sh
@@ -1,9 +1,11 @@
 # check multichain health
+echo check multichain health
 echo {0..5} | tr " " "\n" | xargs -I{} kubectl exec -it multichain-{} -- bin/ash -c 'multichain-cli chain --rpcuser=user --rpcpassword=password getlastblockinfo'
 echo {0..5} | tr " " "\n" | xargs -I{} kubectl exec -it multichain-{} -- bin/ash -c 'multichain-cli chain --rpcuser=user --rpcpassword=password getpeerinfo'
 echo {0..5} | tr " " "\n" | xargs -I{} kubectl exec -it multichain-{} -- bin/ash -c 'multichain-cli chain --rpcuser=user --rpcpassword=password getblockchaininfo'
 
 # check etcd health
+echo check etcd health
 echo {0..5} | tr " " "\n" | xargs -I{} kubectl exec -it etcd-{} -- bin/ash -c 'etcdctl -w=json member list' | jq
 echo {0..5} | tr " " "\n" | xargs -I{} kubectl exec -it etcd-{} -- bin/ash -c 'etcdctl -w=json endpoint status' | jq
 echo {0..5} | tr " " "\n" | xargs -I{} kubectl exec -it etcd-{} -- bin/ash -c 'etcdctl -w=json endpoint health' | jq
